1b08b893df0678b64f265a74841fedc87458d547
Aseem Bansal <aseembansal@live.com>
2015-11-22 18:12:39 +0530

    Demo 08 added

---
+++ src/main/groovy/com/ttnd/im/Demo08_Metaclass_Present.groovy
@@ -0,0 +1,33 @@
+package com.ttnd.im
+
+/**
+ * Demo 08
+ *
+ * Check whether method or property is present
+ */
+
+class Simple {
+
+    String language
+
+    def whatDoYouSpeak() {
+        language
+    }
+
+    def say(String text) {
+        "You say $text in $language"
+    }
+}
+
+
+def s = new Simple(language: 'Groovy')
+
+assert s.metaClass.respondsTo(s, 'whatDoYouSpeak')
+assert Simple.metaClass.respondsTo(s, 'say')
+assert s.metaClass.respondsTo(s, 'say', String)
+assert !s.metaClass.respondsTo(s, 'say', Integer)  // No say(Integer) method.
+assert Simple.metaClass.respondsTo(s, 'toString')  // Method in parent object.
+
+assert s.metaClass.hasProperty(s, 'language')
+assert Simple.metaClass.respondsTo(s, 'getLanguage')  // Get/set methods are generated.
+assert s.metaClass.respondsTo(s, 'setLanguage')
