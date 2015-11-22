0b91e59c3fcceada4b7f6417d6a5423b7616b03a
Aseem Bansal <aseembansal@live.com>
2015-11-22 17:49:53 +0530

    Added exercises

---
+++ src/main/groovy/com/ttnd/im/Demo07_Exercise02.groovy
@@ -0,0 +1,8 @@
+package com.ttnd.im
+
+//TODO Run the demos for per instance metaclass
+//      and make sure you understand what is happening
+
+//TODO Run the Demo05 and see when ExpandoMetaClass is present
+
+//TODO Add new methods to Integer class via Magic Package
